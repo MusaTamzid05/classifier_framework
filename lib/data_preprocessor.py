537f4d24d3627315296b4c7fd252b14797378fc3
MusaTamzid05 <tamzid101@gmail.com>
2021-10-22 23:02:43 +0600

    loading the images and labels for dir.

--- lib/data_preprocessor.py
+++ lib/data_preprocessor.py
@@ -1,5 +1,8 @@
 import cv2
+import os
 from tensorflow.keras.preprocessing.image import img_to_array
+
+import numpy as np
 
 class DataPreprocessor:
     def __init__(self, image_size, dir_path = None):
@@ -17,3 +20,24 @@ class DataPreprocessor:
         image = img_to_array(image)
 
         return image
+
+    def fit(self):
+        images, labels = [], []
+
+        for label_dir in os.listdir(self.dir_path):
+            print(f"Loading from {label_dir}")
+            current_image = self._load_images_from(dir_path = os.path.join(self.dir_path, label_dir))
+            images += current_image
+            labels += [label_dir for i in range(len(current_image))]
+
+
+
+
+    def _load_images_from(self, dir_path):
+        images = []
+        for image_name in os.listdir(dir_path):
+            image_path = os.path.join(dir_path, image_name)
+            image = self.process_image(image = image_path)
+            images.append(image)
+
+        return images
--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from lib.data_preprocessor import DataPreprocessor
 
 def main():
-    preprocessor = DataPreprocessor(image_size = 24)
-    image = preprocessor.process_image(image = "/home/musa/m3.jpg")
-
-    print(image.shape)
+    preprocessor = DataPreprocessor(image_size = 24, dir_path = "/home/musa/python_pro/custom_baracuda_test/create_data/caltech")
+    preprocessor.fit()
 
 
 if __name__ == "__main__":
