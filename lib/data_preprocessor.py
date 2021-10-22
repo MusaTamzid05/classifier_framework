af25accb581776e9384cdb9ca4dee103a97ef013
MusaTamzid05 <tamzid101@gmail.com>
2021-10-22 23:39:55 +0600

    label now encoded.

--- lib/data_preprocessor.py
+++ lib/data_preprocessor.py
@@ -1,13 +1,17 @@
 import cv2
 import os
 from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.utils import to_categorical
 
 import numpy as np
+
+from sklearn.preprocessing import LabelEncoder
 
 class DataPreprocessor:
     def __init__(self, image_size, dir_path = None):
         self.dir_path = dir_path
         self.image_size = image_size
+        self.num_classes = None
 
 
     def process_image(self, image):
@@ -23,7 +27,11 @@ class DataPreprocessor:
     def fit(self):
         images, labels = self._load_image_and_labels()
 
+        self.num_classes = len(np.unique(labels))
+        labels = self._process_labels(labels = labels)
+
         print(images.shape, labels.shape)
+
 
 
     def _load_image_and_labels(self):
@@ -34,7 +42,6 @@ class DataPreprocessor:
             current_image = self._load_images_from(dir_path = os.path.join(self.dir_path, label_dir))
             images += current_image
             labels += [label_dir for i in range(len(current_image))]
-
 
         return np.array(images), np.array(labels)
 
@@ -50,3 +57,8 @@ class DataPreprocessor:
             images.append(image)
 
         return images
+
+    def _process_labels(self, labels):
+        self.label_encoder = LabelEncoder().fit(labels)
+        return to_categorical(self.label_encoder.transform(labels), self.num_classes)
+
